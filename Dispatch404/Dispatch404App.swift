1a46e853e04128e46c020a3b9ca7207fdb384492
Shawn Tucker <shqtucke@gmail.com>
2024-12-04 06:45:35 -0500

    Initial Commit

---
+++ Dispatch404/Dispatch404App.swift
@@ -0,0 +1,17 @@
+//
+//  Dispatch404App.swift
+//  Dispatch404
+//
+//  Created by Shawn Tucker on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Dispatch404App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dispatch404Tests/Dispatch404Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Dispatch404Tests.swift
+//  Dispatch404Tests
+//
+//  Created by Shawn Tucker on 12/4/24.
+//
+
+import Testing
+
+struct Dispatch404Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
